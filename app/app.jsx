f1a0bf1a0538dd4185afcc58ec798be09b72a4a8
James Jillians <jjillians@jjillians-mbp.local>
2016-08-25 22:16:44 +0100

    First version of playpen

---
+++ app/app.jsx
@@ -0,0 +1,11 @@
+//Access your JavaScript here!!!
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from './stuff/helloWorld/HelloWorld.jsx';
+
+ReactDOM.render(
+    <HelloWorld>Hello World!</HelloWorld>,
+    document.getElementById('app')
+);
---
+++ app/stuff/helloWorld/HelloWorld.jsx
@@ -0,0 +1,11 @@
+/**
+ * React Component that displays Hello World message
+ */
+
+import React from 'react';
+
+const HelloWorld = (props) => {
+    return <div className="hello-msg">Hello World!</div>;
+};
+
+export default HelloWorld;
